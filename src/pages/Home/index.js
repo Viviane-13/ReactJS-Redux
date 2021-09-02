467436735ad228fbcaf58732ce42ea8ef71d6d7f
Viviane <oviviane772@gmail.com>
2021-09-02 10:28:11 -0300

    Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { MdFlightTakeoff } from "react-icons/md";
+
+import api from "../../services/api";
+import "./style.css";
 
 export default function Home() {
- return (
-   <div>
-     <h1>Home</h1>
-   </div>
- );
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    async function loadApi() {
+      const response = await api.get("trips");
+      setTrips(response.data);
+    }
+    loadApi();
+  }, []);
+  return (
+    <div>
+      <div className="box">
+        {trips.map((trip) => (
+          <li key={trip.id}>
+            <img src={trip.image} alt={trips.title} />
+            <strong>{trip.title}</strong>
+            <span>Status: {trip.status ? "Disponível" : "Indisponível"}</span>
+            <button type="button" onClick={() => {}}>
+              <div>
+                <MdFlightTakeoff size={16} color="#FFF" />
+              </div>
+              <span>Solicitar reserva</span>
+            </button>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
 }
