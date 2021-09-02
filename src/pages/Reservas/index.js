bfc85e9e4a41a261e8c7eed528fdd653d2185343
Viviane <oviviane772@gmail.com>
2021-09-02 11:03:21 -0300

    reservas

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import { MdDelete } from "react-icons/md";
+import "./style.css";
 
 export default function Reservas() {
- return (
-   <div>
-     <h1>Reservas</h1>
-   </div>
- );
+  return (
+    <div>
+      <h1 className="title">Você solicitou 1 reserva(s)</h1>
+      <div className="reservas">
+        <img
+          src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
+          alt="Maceio"
+        />
+        <strong>Viagem Maceio 7 dias</strong>
+        <span>Quantidade: 2</span>
+        <button type="button" onClick={() => {}}>
+          <MdDelete size={20} color="#191919" />
+        </button>
+      </div>
+      <footer>
+        <button type="button">Solicitar Reservas</button>
+      </footer>
+    </div>
+  );
 }
